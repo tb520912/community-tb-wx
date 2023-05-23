1581a9754c3d491496e816b4bbb359428f3b4373
田斌 <tianbin0912@163.com>
2023-05-23 10:55:35 +0800

    token刷新

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: 田斌 tianbin0912@163.com
+ * @Date: 2023-05-22 16:06:18
+ * @LastEditors: 田斌 tianbin0912@163.com
+ * @LastEditTime: 2023-05-23 10:48:07
+ * @FilePath: \wx_app\enjoy-plus\miniprogram\app.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 // app.js
 import './utils/utils.js'
 import './utils/http.js'
@@ -9,6 +17,7 @@ App({
   },
   getToken () {
     this.token = wx.getStorageSync('token')
+    this.refreshToken = wx.getStorageSync('refreshToken')
     console.log(this.token, 'this.token');
   },
   setToken (key, token) {
--- miniprogram/utils/http.js
+++ miniprogram/utils/http.js
@@ -15,6 +15,52 @@ http.intercept.request = (options) => {
 }
 // 响应拦截器
 http.intercept.response = (res) => {
+    if (res.statusCode === 401) {
+        // 401 未授权
+        // 1. 本地没有token, 直接跳转到登录页面
+        if (!getApp().token) {
+            wx.navigateTo({
+                url: '/pages/login/index'
+            })
+        }
+        // 2. 本地有token, 但是token过期了, 则使用refreshToken去换取新的token
+        else {
+            // 2.1 本地没有refreshToken, 则跳转到登录页面
+            if (!getApp().refreshToken) {
+                wx.navigateTo({
+                    url: '/pages/login/index'
+                })
+            }
+            // 2.2 本地有refreshToken, 则使用refreshToken去换取新的token
+            else {
+                // 2.2.1 使用refreshToken去换取新的token
+                http({url: '/refreshToken', method: 'post', header: {Authorization: 'Bearer ' + getApp().refreshToken}}).then((res) => {
+                    if (res.statusCode === 200) {
+                        // 2.2.2 更新本地token
+                         getApp().setToken('token', res.data.token)
+                         getApp().setToken('refreshToken', res.data.refreshToken)
+                        // 2.2.3 重新发起请求
+                        // 1.2 获取到原来接口请求的参数
+                        res.config = Object.assign(res.config, {
+                            header: {
+                            // 更新后的 token
+                            Authorization: 'Bearer ' + res.data.token,
+                            },
+                        })
+                        // 重新发请求
+                        http(res.config)
+                    }
+                    else {
+                        // 2.2.4 重新登录
+                        wx.navigateTo({
+                            url: '/pages/login/index'
+                        })
+                    }
+                })
+            }
+        }
+    }
+
     if (res.statusCode === 200) {
         return res.data
     }
